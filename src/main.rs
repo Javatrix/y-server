8a48c31a4f8a763b5c3c445e3333202910f736d5
Javatrix <javatrix912@gmail.com>
2024-07-24 12:12:41 +0200

    Adding users prototype

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use model::post::Post;
+use dotenvy::dotenv;
+use model::{
+    post::Post,
+    user::{User, UserCreationResult, UserPayload},
+};
 use serde::Deserialize;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 
-use std::sync::Mutex;
+use std::{env, sync::Mutex};
 
 pub mod model;
 
 struct AppState {
+    pool: PgPool,
     posts: Mutex<Vec<Post>>,
 }
 
@@ -30,9 +36,41 @@ async fn get_posts(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(&*posts)
 }
 
+async fn create_user(data: web::Data<AppState>, payload: web::Json<UserPayload>) -> impl Responder {
+    let result = sqlx::query!(
+        "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id",
+        payload.username,
+        payload.password
+    )
+    .fetch_one(&data.pool)
+    .await;
+
+    match result {
+        Ok(record) => {
+            let response = UserCreationResult { id: record.id };
+            HttpResponse::Created().json(response)
+        }
+        Err(err) => {
+            HttpResponse::InternalServerError().body(format!("Failed to create user: {}", err))
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().expect("No .env file found");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(
+            &env::var("DATABASE_URL")
+                .expect("DATABASE_URL must be specified within the .env file."),
+        )
+        .await
+        .unwrap();
+
     let state = web::Data::new(AppState {
+        pool,
         posts: Mutex::new(Vec::new()),
     });
 
@@ -41,6 +79,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(state.clone())
             .route("/post", web::post().to(post))
             .route("/get-posts", web::get().to(get_posts))
+            .route("/new-user", web::post().to(create_user))
     })
     .bind("127.0.0.1:8080")?
     .run()
---
+++ src/model/user.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+
+#[derive(FromRow, Deserialize, Serialize)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct UserPayload {
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Serialize)]
+pub struct UserCreationResult {
+    pub id: i32,
+}
